824904ac7c2088f515f41c3ded3c90c4c139f111
Denzell Grant <denzgrant@gmail.com>
2020-05-21 22:40:46 -0700

    separated connection file

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+const util = require("util");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "characters_db"
+});
+
+connection.connect(function (error) {
+    if (error) {
+        throw error;
+    }
+    console.log(`Connected to database as id ${connection.threadId}`);
+});
+// Needed for SQL One 
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
